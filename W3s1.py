d9a39ee0bb1e423a79fd3c12f0d5d2d448a67ebd
jalvespinto <jalvespinto@gmail.com>
2021-08-25 14:01:11 -0300

    correct GET sintax

---
+++ W3s1.py
@@ -0,0 +1,15 @@
+import socket
+import urllib.parse
+
+fhand = 'http://data.pr4e.org/romeo.txt'
+url = urllib.parse.urlparse(fhand)
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect((url.hostname, 80))
+cmd = 'GET /data.pr4e.org/romeo.txt HTTP/1.1\r\nHost: www.data.pr4e.org\r\n\r\n'.encode()
+mysock.send(cmd)
+while True:
+    data = mysock.recv(512)
+    if (len(data) < 1):
+        break
+    print(data.decode())
+mysock.close()
